4b8147c1eff204491ee8dab0a8c78e56486cbddc
Papa Gyeke Lartey <papa.gyekelartey@hcl.com>
2022-06-05 11:09:02 -0600

    End of secton 4 (fix API issue)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,35 +1,26 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements OnInit{
-  title = 'The Dating App';
+export class AppComponent implements OnInit {
+  title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getUsers();
   }
-  /*
-  getUsers(){
-    this.http.get('https:localhost:5001/api/users').subscribe(response =>{
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
       this.users = response;
-    },error=>{
+    }, error => {
       console.log(error);
     })
   }
-  */
-  
-  getUsers(){
-    this.http.get('https:localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error:error => console.log(error)
-    })
-  }
-  
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
