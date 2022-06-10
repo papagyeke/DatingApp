43e805539e13d3230f07dd74889ca06bfee11b19
Papa Gyeke Lartey <papa.gyekelartey@hcl.com>
2022-06-10 02:26:54 -0600

    End of section 8

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        // public MemberDto(int id, int age, DateTime created, DateTime lastActive, string introduction, string interests, string country)
+        // {
+        //     this.Id = id;
+        //     this.Age = age;
+        //     this.Created = created;
+        //     this.LastActive = lastActive;
+        //     this.Introduction = introduction;
+        //     this.Interests = interests;
+        //     this.Country = country;
+
+        // }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
+
+        public int Age { get; set; } //make sure the property function in the entity has a GetAge
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
